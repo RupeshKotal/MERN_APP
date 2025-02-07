9c1b0652df6b88b221fcde9cb36836fd35236d65
RupeshKotal <rupeshkotal99@gmail.com>
2025-02-07 12:47:45 +0530

    product

--- backend/Product/server.js
+++ backend/Product/server.js
@@ -1,7 +1,16 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const mongoose = require("mongoose");
 
+const MONGO_URI = "mongodb://admin:password@mongodb:27017/ecommerce?authSource=admin";
+
+mongoose.connect(MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+    .then(() => console.log("✅ MongoDB Connected Successfully"))
+    .catch(err => console.error("❌ MongoDB Connection Error:", err));
 
 require('dotenv').config();
 require('./config/db_conn');
